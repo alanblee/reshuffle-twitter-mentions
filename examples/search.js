6b58dbdc36dee91690359a4db22d6e6193665d18
alanblee <alanbenlee12@gmail.com>
2020-11-02 16:26:50 -0800

    able to connect to twitter and search for tweets

--- examples/search.js
+++ examples/search.js
@@ -1,1 +1,27 @@
 require("dotenv").config();
+const { Reshuffle } = require("reshuffle");
+const { TwitterConnector } = require("reshuffle-twitter-connector");
+
+(async () => {
+  const app = new Reshuffle();
+  const twitter = new TwitterConnector(app, {
+    customerKey: process.env.TWITTER_CUSTOMER_KEY,
+    customerSecret: process.env.TWITTER_CUSTOMER_SECRET,
+  });
+
+  twitter.on({ search: "@reshuffleHQ" }, async (event, app) => {
+    const tweetsCache = {};
+    for (const tweet of event.tweets) {
+      if (!tweetsCache[tweet.id]) {
+        tweetsCache[tweet.id] = {
+          user: tweet.user.screen_name,
+          date: tweet.created_at,
+          tweet: tweet.text,
+        };
+      }
+    }
+    console.log(tweetsCache);
+  });
+
+  app.start(8000);
+})().catch(console.error);
