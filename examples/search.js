04bcdba018573b4e6067fcd47f0164359d6844eb
alanblee <alanbenlee12@gmail.com>
2020-11-03 17:53:35 -0800

    monday.sdk formatting issues, using axios post instead

--- examples/search.js
+++ examples/search.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const axios = require("axios");
 const { Reshuffle } = require("reshuffle");
 const { TwitterConnector } = require("reshuffle-twitter-connector");
 const { MondayConnector } = require("reshuffle-monday-connector");
@@ -14,21 +15,78 @@ const { MondayConnector } = require("reshuffle-monday-connector");
   const tweetsCache = {};
 
   // creates new item on specific board. board ID comes from url in browser
-  const createItems = async (tweetId) => {
-    const board = await monday.createItem(BOARD_ID, tweetId);
+  const createItems = async (tweetInfo) => {
+    const column = await monday
+      .sdk()
+      .api(`query {boards (ids: ${BOARD_ID}) { columns { id title type } }}`)
+      .then((res) => {
+        // console.log(res.data.boards[0].columns);
+        return res.data.boards[0].columns.map((col) => {
+          return col.id;
+        });
+      })
+      .then(async (col) => {
+        // const newItems = await monday.sdk().api(
+        //   `mutation {create_item (board_id: ${BOARD_ID}, item_name: \"${
+        //     tweetInfo.id
+        //   }\", column_values:
+        //       \"{
+        //         \\\"${col[1]}\\\":\\\"${tweetInfo.text.replace(/\n/g, "")}\\\",
+        //         \\\"${col[2]}\\\":\\\"${tweetInfo.user.screen_name}\\\",
+        //         \\\"${col[3]}\\\":\\\"{\\\"date\\\":\\\"2019 - 01 - 20\\\"}\\\"
+        //       }\") {id}}`
+        // );
+        //   console.log(newItems);
+        const body = {
+          query: `
+          mutation ($boardId: Int!,, $itemName: String!, $columnValues: JSON!) {
+            create_item (
+              board_id: $boardId,
+              item_name: $itemName,
+              column_values: $columnValues
+            ) {
+              id
+            }
+          }
+          `,
+          variables: {
+            boardId: BOARD_ID,
+            itemName: JSON.stringify(tweetInfo.id),
+            columnValues: JSON.stringify({
+              tweet17: tweetInfo.text,
+              text: tweetInfo.user.screen_name,
+              created_at: {
+                date: new Date(Date.parse(tweetInfo.created_at))
+                  .toISOString()
+                  .split("T")[0],
+              },
+            }),
+          },
+        };
+        axios
+          .post(`https://api.monday.com/v2`, body, {
+            headers: {
+              Authorization: process.env.MONDAY_TOKEN,
+            },
+          })
+          .catch((err) => {
+            console.error("** error **", err.data);
+          })
+          .then((res) => {
+            console.log("** success **", res.data);
+          });
+      });
   };
 
-  const getBoardItems = async () => {
+  (async () => {
     const board = await monday.getBoard(BOARD_ID);
     board.boards[0].items.forEach(async (item) => {
       const itemName = await monday.getItem(Number(item.id));
-      // console.log(itemName.items[0].name);
       if (!tweetsCache[Number(itemName.items[0].name)]) {
         tweetsCache[Number(itemName.items[0].name)] = { fetched: true };
       }
     });
-  };
-  getBoardItems();
+  })().catch(console.error);
 
   twitter.on({ search: "@reshuffleHQ" }, async (event, app) => {
     for (const tweet of event.tweets) {
@@ -38,10 +96,9 @@ const { MondayConnector } = require("reshuffle-monday-connector");
           date: tweet.created_at,
           tweet: tweet.text,
         };
-        createItems(JSON.stringify(tweet.id));
+        createItems(tweet);
       }
     }
-    console.log(tweetsCache);
   });
 
   app.start(8000);
